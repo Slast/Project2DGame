3b01344213ff8ea08a1d8e03ec8ff844cdfb3ac7
Slast <mcmurach23@yahoo.de>
2017-04-20 17:34:55 +0200

    Project creation
    Creation of the project as an empty default UE4 code project.

---
+++ Source/Project2DGame/Project2DGameGameModeBase.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Project2DGame.h"
+#include "Project2DGameGameModeBase.h"
+
+
+
+
---
+++ Source/Project2DGame/Project2DGameGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "Project2DGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT2DGAME_API AProject2DGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
